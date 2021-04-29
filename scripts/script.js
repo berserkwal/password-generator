7bb4bd6b78cac23ef20d4d11b3451ac8ef25cce6
berserkwal <kharkwalvikas@gmail.com>
2021-04-29 15:58:18 +0530

    Connected the logic to respective DOM element. Also created functionality for copying to clipboard and randomizing buttons and the slider funtionality to change the desired length. Also made the tooltip move with the slider.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+const passwordText = document.querySelector(".text-box span");
+const slider = document.querySelector("#password-range");
+const randomize = document.querySelector(".randomize-button");
+const copy = document.querySelector(".copy-button");
+const rangeHover = document.querySelector(".value-hover");
+const copyTooltip = document.querySelector(".copy-tooltip");
+const randomizeTooltip = document.querySelector(".randomize-tooltip");
+let passwordLength = slider.value;
+
 const validCharacters =
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=[]{};:',./?";
 
@@ -12,4 +21,28 @@ function generatePassword(length) {
 	return result;
 }
 
-console.log(generatePassword(32))
+passwordText.innerText = generatePassword(passwordLength);
+
+randomize.addEventListener("click", () => {
+	passwordText.innerText = generatePassword(passwordLength);
+	randomizeTooltip.classList.add("visible");
+
+	setTimeout(() => randomizeTooltip.classList.remove("visible"), 1000);
+});
+
+copy.addEventListener("click", () => {
+	navigator.clipboard.writeText((text = passwordText.innerText));
+	copyTooltip.classList.add("visible");
+
+	setTimeout(() => copyTooltip.classList.remove("visible"), 1000);
+});
+
+slider.addEventListener("input", () => {
+	passwordLength = slider.value;
+	rangeHover.innerText = passwordLength;
+	passwordText.innerText = generatePassword(passwordLength);
+	// rangeHover.style.left = `${slider.value - slider.min}`;
+	var left = (100 / 58.5) * (slider.value - slider.min);
+	rangeHover.style.left = `${left}%`;
+	// rangeHover.style.transform = `translateX(${-5}px)`;
+});
